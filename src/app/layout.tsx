96f8f4a808cedde0e51f26c17fe93fbcdd22dc9f
Salvador <salvador.karakachoff@gmail.com>
2024-01-15 01:10:41 -0300

    Remove unnecessary empty lines

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,6 @@ import './globals.css'
 import Footer from '@/components/Footer'
 import Header from '@/components/header/Header'
 
-
 export const metadata: Metadata = {
   title: 'Salvador Karakachoff - Computer Engineering and Data Science',
   description: 'Web Developer with over 3 years of experience. IT Support. Advanced student in Computer Engineering and Data Science.',
@@ -17,7 +16,6 @@ export default function RootLayout({
 
   return (
     <html lang="en" className="dark">
-
 
       <body  className="bg-slate-100/90 dark:bg-neutral-900 overflow-x-hidden">
         <Header />
