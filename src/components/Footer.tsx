41468d3218580b9d34e8e05d4934263d0aa64751
Salvador <salvador.karakachoff@gmail.com>
2024-01-15 01:11:08 -0300

    Update Footer styling

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function Footer() {
   return (
-    <footer className="p-10 self-center font-semibold text-center text-black/90 dark:text-white/90 bg-neutral-300/80 dark:bg-neutral-900 w-full">
+    <footer className="p-10 self-center font-semibold text-center text-black/90 dark:text-white/90 bg-neutral-300/30 dark:bg-neutral-950/30 w-full">
         <p>© 2024 <Link href="/" className="text-purple-500 hover:underline">Salvador Karakachoff</Link></p>
         <p>Using Next.js, React, Tailwind y TypeScript</p>
     </footer>
