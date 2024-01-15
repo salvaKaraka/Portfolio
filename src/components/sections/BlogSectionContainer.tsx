4183021d363576f00aceab06cd61e842efcc0127
Salvador <salvador.karakachoff@gmail.com>
2024-01-15 01:11:41 -0300

    Update styles in BlogSectionContainer

--- src/components/sections/BlogSectionContainer.tsx
+++ src/components/sections/BlogSectionContainer.tsx
@@ -9,15 +9,15 @@ type SectionProps = {
 
 export default function SectionContainer({ children, className, title, subtitle }: SectionProps) {
   return (
-    <section className={`mx-auto max-w-6xl p-4 md:p-0 text-black/90 dark:text-white/90 divide-slate-300 dark:divide-neutral-700 ${className}`}>
+    <section className={` mx-auto max-w-6xl p-4 md:p-0 text-black/90 dark:text-white/90 divide-slate-300 dark:divide-neutral-700 ${className}`}>
 
-      <div className="my- text-center max-w-4xl mx-auto h-20">
+      <div className="my-8 text-center max-w-4xl mx-auto h-20">
 
-        <h1 className="text-2xl text-slate-600 dark:text-slate-300 ">{title}</h1>
-        <p className="text-slate-400 mt-2">{subtitle}</p>
+        <h1 className="text-6xl font-medium text-slate-700 dark:text-slate-200 ">{title}</h1>
+        <p className="text-xl text-slate-400 mt-2">{subtitle}</p>
       </div>
 
-      <hr className="my-8" />
+      <hr className="my-20" />
 
       {children}
     </section>
