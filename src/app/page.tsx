9ed59d0fd51baa1efaac6c4d2d9d70d920708f5d
Salvador <salvador.karakachoff@gmail.com>
2024-01-15 01:10:50 -0300

    Add Introduction component to Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import Chatbot from '@/components/Chatbot'
+import SectionContainer from '@/components/sections/SectionContainer'
+import Introduction from '@/components/home/Introduction'
 
 export default function Home() {
   return (
     <main className="p-4 min-h-screen">
-      <Chatbot />
+      <Introduction />
+      <Chatbot /> 
     </main>
   )
 }
