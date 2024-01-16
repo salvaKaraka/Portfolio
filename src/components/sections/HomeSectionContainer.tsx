a63341caaa1242d9dcb8f6b8285e8b0a75924815
Salvador <salvador.karakachoff@gmail.com>
2024-01-16 18:20:20 -0300

    Update styling in HomeSectionContainer

--- src/components/sections/HomeSectionContainer.tsx
+++ src/components/sections/HomeSectionContainer.tsx
@@ -8,10 +8,10 @@ type props = {
 export default function Projects({title, children}: props) {
     
     return (
-        <SectionContainer className="divide-slate-300 my-10 dark:divide-neutral-700">
+        <SectionContainer className="divide-violet-400">
             <div className="flex flex-row items-center justify-evenly mb-14">
-                <hr className="w-1/2 my-4 border-gray-900 dark:border-gray-100" />
-                <h1 className="text-6xl font-medium text-gray-900 dark:text-gray-100">
+                <hr className="hidden md:block w-1/2 my-4 border-violet-400" />
+                <h1 className="text-6xl md:text-6xl font-medium text-gray-900 dark:text-gray-100">
                     {title}
                 </h1>
             </div>
