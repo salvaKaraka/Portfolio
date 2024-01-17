d14110b48a95a91f2755d11bf86acd3e7b3c231c
Salvador <salvador.karakachoff@gmail.com>
2024-01-17 02:40:03 -0300

    Hide 3d models and stop their animations when not on view

--- src/components/home/projects/ModelCanvas.tsx
+++ src/components/home/projects/ModelCanvas.tsx
@@ -1,25 +1,70 @@
 "use client"
 import { Canvas } from "@react-three/fiber";
 import { ContactShadows, Environment, OrbitControls } from "@react-three/drei";
-import { Suspense } from "react";
+import { Suspense, useEffect, useRef, useState } from "react";
+import { useInView } from "react-intersection-observer";
 
 type Model3DProps = {
-    children: React.ReactNode;
-    height: string;
-    shadowPosition?: [number, number, number];
-}
+  children: React.ReactNode;
+  height: string;
+  shadowPosition?: [number, number, number];
+};
 
-export default function Model3D({children, height, shadowPosition}: Model3DProps){
-    
-    return(
-        <Canvas style={{height:`${height}`, width:"10rem"}}>
+export default function Model3D({
+  children,
+  height,
+  shadowPosition,
+}: Model3DProps) {
+  const [ref, inView] = useInView();
+  const canvasRef = useRef<HTMLCanvasElement | null>(null);
+
+  const [rotation, setRotation] = useState(0)
+
+  useEffect(() => {
+    const canvasElement = canvasRef.current;
+  
+    if (canvasElement) {
+      if (inView) {
+        const interval = setInterval(() => {
+          setRotation((prevRotation) => prevRotation + .02);
+        }, 60);
+        return () => clearInterval(interval);
+      }
+    }
+  }, [inView]);
+
+  return (
+    <div ref={ref} style={{ height: `${height}`, width: "10rem" }}>
+      <Canvas
+        ref={(canvas) => {
+          canvasRef.current = canvas as HTMLCanvasElement;
+        }}
+        onCreated={({ gl }) => {
+          // Configuraciones iniciales si es necesario
+        }}
+      >
+        {inView && (
+          <>
             <ambientLight intensity={0} />
-            <OrbitControls enableZoom={false}/>
+            <OrbitControls enableZoom={false} />
             <Suspense fallback={null}>
-            {children}
+            <mesh scale={1.16} rotation={[0,rotation,0]}>
+                {children}
+            </mesh>
             </Suspense>
             <Environment preset="sunset" />
-            <ContactShadows position={shadowPosition} opacity={1} scale={10} blur={3} far={10} resolution={256} color="#000"/>
-        </Canvas>
-    )
+            <ContactShadows
+              position={shadowPosition}
+              opacity={1}
+              scale={10}
+              blur={3}
+              far={10}
+              resolution={256}
+              color="#000"
+            />
+          </>
+        )}
+      </Canvas>
+    </div>
+  );
 }
