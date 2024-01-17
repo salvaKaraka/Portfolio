3fa56e318ea8d78eacfb15f86fd06235344e7e3d
Salvador <salvador.karakachoff@gmail.com>
2024-01-17 02:38:47 -0300

    Removed blur for optimization

--- src/app/blog/loading.tsx
+++ src/app/blog/loading.tsx
@@ -6,7 +6,7 @@ export default async function BlogLoading() {
     const postMetadata = await postMetadataPromise;  // Esperamos a que la promesa se resuelva
 
     const placeholders = postMetadata.map((post) => (
-        <div key={post.title} className={`border backdrop-blur-2xl border-neutral-700 animate-pulse p-2 rounded-xl h-32 transition-all`}>
+        <div key={post.title} className={`border border-neutral-700 animate-pulse p-2 rounded-xl h-32 transition-all`}>
         </div>
     ));
 
--- src/components/home/projects/project-card/ProjectCard.tsx
+++ src/components/home/projects/project-card/ProjectCard.tsx
@@ -19,17 +19,8 @@ type props = {
 
 export default function PostCard({ title, description, link, github, model, tags, shadowPosition}: props) {
 
-    const [rotation, setRotation] = useState(0)
-
-    useEffect(() => {
-        const interval = setInterval(() => {
-            setRotation(rotation => rotation + 0.003)
-        }, 10);
-        return () => clearInterval(interval);
-    }, []);
-
     return (
-            <div className='w-full md:w-[90%] m-auto border backdrop-blur-2xl border-neutral-400 dark:border-neutral-700 p-2 rounded-xl'>
+            <div className='w-full md:w-[90%] m-auto border border-neutral-400 dark:border-neutral-700 p-2 rounded-xl'>
                 <div className='flex flex-col md:flex-row '>
                     <div className='p-4'>
                     <Link href="/" className="group flex text-2xl hover:text-purple-600 transition">
@@ -45,10 +36,8 @@ export default function PostCard({ title, description, link, github, model, tags
                     </div>
                     <div className="cursor-grab active:cursor-grabbing hover:scale-110 lg:mr-16 transition-all items-center justify-center mx-auto">
                         {model &&
-                            <ModelCanvas height="10rem" shadowPosition={shadowPosition}>
-                                <mesh scale={1.16} rotation={[0,rotation,0]}>
+                            <ModelCanvas height="10rem" shadowPosition={shadowPosition}>         
                                     {model}
-                                </mesh>
                             </ModelCanvas>
                         }
                     </div>
