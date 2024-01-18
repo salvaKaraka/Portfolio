2867b67b8da90ea2ee93b490efc37cf462e244d3
Salvador <salvador.karakachoff@gmail.com>
2024-01-18 00:40:24 -0300

    Added Link to my CV

--- src/components/SocialPil.tsx
+++ src/components/SocialPil.tsx
@@ -8,7 +8,7 @@ type props ={
 
 export default function SocialTag({children, className, href}:props){
     return (
-        <a target="_blank" rel="noopener noreferrer" href={href} className={`${className? className : ""} cursor-pointer rounded-full font-semibold flex justify-center items-center gap-x-1 md:gap-x-2 py-1 md:py-2 px-2 md:px-3 border border-black/10 bg-black/5 hover:bg-black/10 dark:border-white/5 dark:bg-white/10 dark:hover:bg-white/30 hover:scale-110 transition`}>
+        <a target="_blank" rel="noopener noreferrer" href={href} className={`${className? className : ""} text-sm cursor-pointer rounded-full font-semibold flex justify-center items-center gap-x-1 md:gap-x-2 py-1 px-2 border border-black/10 hover:bg-black/10 dark:border-white/5 dark:hover:bg-white/5 transition`}>
             {children}
         </a>
     )
