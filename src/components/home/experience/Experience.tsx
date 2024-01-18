17a6afb99c9917639eeeb99f51433e8e380243f2
Salvador <salvador.karakachoff@gmail.com>
2024-01-18 01:03:23 -0300

    Add ExperienceItem component and update Experience.tsx and HomeSectionContainer.tsx

--- src/components/home/experience/Experience.tsx
+++ src/components/home/experience/Experience.tsx
@@ -1,11 +1,42 @@
 import HomeSectionContainer from "../../sections/HomeSectionContainer";
+import ExperienceItem from "./ExperienceItem";
+
+const EXPERIENCE = [
+    {
+        title: 'IT Support - Faculty of Engineering, National University of La Plata',
+        date: '2022 - Now',
+        description: 'Providing technical support to the entire administrative and teaching staff of the faculty involves implementing, maintaining, and configuring telephone and internet networks with over 2000 devices. Alongside this, I handle the repair of computers and electronic devices. Addressing each unique problem demands improvisation and on-the-fly learning, all while maintaining a professional approach.',
+    },
+    {
+        title: 'Private Tutor',
+        date: '2021 - Now',
+        description: 'Offering private lessons to students across various academic levels, I specialize in mathematics, English, and computer science. This experience has significantly enhanced not only my communication and teaching skills but also my understanding of diverse learning needs.',
+    },
+    {
+        title: 'Web Development - Freelance',
+        date: '2016 - Now',
+        description: 'Engaged in freelance web development, I am responsible for designing and developing web applications. Examples of my work can be explored on my GitHub or in the projects section, showcasing a range of projects and technical capabilities.',
+        link: "https://github.com/salvaKaraka"
+    }
+] 
+
 
 
 export default function Experience() {
 
     return (
         <HomeSectionContainer title="Experience">
-                <></>
+                <ol className="relative border-s border-gray-500 dark:border-gray-200 ml-3">                  
+    
+                    {EXPERIENCE.map(experience => (
+                        <li className="mb-10 ms-4">
+                            <ExperienceItem {...experience}/>    
+                        </li>
+                    ))}
+
+                </ol>
         </HomeSectionContainer>
     )
 }
+
+
---
+++ src/components/home/experience/ExperienceItem.tsx
@@ -0,0 +1,42 @@
+
+
+interface props {
+    title: string;
+    description: string;
+    link?: string;
+    date: string;
+}
+
+export default function ExperienceItem ({ title, description, link, date } : props){
+
+
+    return (
+        <>
+    <div className="absolute w-3 h-3 dark:bg-gray-200 rounded-full mt-1.5 -start-1.5 border dark:border-white border-gray-900 bg-gray-700">
+    </div>
+    <time
+        className="mb-1 text-sm font-normal leading-none text-gray-500 dark:text-gray-400"
+        >{date}</time
+    >
+    <h3 className="text-lg font-semibold text-purple-800/90 dark:text-purple-400/90">
+        {title}
+    </h3>
+    <p className="mb-4 text-base font-normal dark:text-gray-200 text-gray-800">
+        {description}
+    </p>
+    {
+        link && (
+            <a
+            href={link}
+            className="border font-semibold border-black/10 bg-black/5 hover:bg-black/10 dark:border-white/5 dark:bg-white/10 dark:hover:bg-white/30 rounded-full
+    inline-flex justify-center items-center
+    py-1 px-2 md:py-2 md:px-4
+    text-xs md:text-base
+    transition
+    hover:scale-110 "
+            >See more ↗</a>
+        )
+    }
+    </>
+    )
+}
