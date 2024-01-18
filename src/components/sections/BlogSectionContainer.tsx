858734a44c266786b6c886c5776151903272b606
Salvador <salvador.karakachoff@gmail.com>
2024-01-18 01:48:44 -0300

    Update BlogSectionContainer styling

--- src/components/sections/BlogSectionContainer.tsx
+++ src/components/sections/BlogSectionContainer.tsx
@@ -11,13 +11,13 @@ export default function SectionContainer({ children, className, title, subtitle 
   return (
     <section className={` mx-auto w-full md:max-w-6xl p-4 md:p-0 text-black/90 dark:text-white/90 divide-slate-300 dark:divide-neutral-700 ${className}`}>
 
-      <div className="my-8 text-center w-full md:max-w-4xl mx-auto h-20">
+      <div className="mb-16 mt-20 text-center w-full md:max-w-4xl mx-auto h-20">
 
         <h1 className="text-5xl font-medium text-neutral-900 dark:text-neutral-100 ">{title}</h1>
-        <p className="text-lg font-medium text-neutral-600 dark:text-neutral-400 mt-6">{subtitle}</p>
+        <p className="text-lg font-medium text-neutral-600 dark:text-neutral-400 mt-4">{subtitle}</p>
       </div>
 
-      <hr className="my-20" />
+      <hr className="mb-8" />
 
       {children}
     </section>
