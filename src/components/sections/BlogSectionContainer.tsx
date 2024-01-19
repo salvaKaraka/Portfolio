c4ab98ec33350e506903baad4553ab9d4cc22c77
Salvador <salvador.karakachoff@gmail.com>
2024-01-18 23:49:50 -0300

    Refactor layout and introduction components

--- src/components/sections/BlogSectionContainer.tsx
+++ src/components/sections/BlogSectionContainer.tsx
@@ -9,9 +9,9 @@ type SectionProps = {
 
 export default function SectionContainer({ children, className, title, subtitle }: SectionProps) {
   return (
-    <section className={` mx-auto w-full md:max-w-6xl p-4 md:p-0 text-black/90 dark:text-white/90 divide-slate-300 dark:divide-neutral-700 ${className}`}>
+    <section className={`my-5 md:my-8 mx-auto w-full md:max-w-6xl p-4 md:p-0 text-black/90 dark:text-white/90 divide-slate-300 dark:divide-neutral-700 ${className}`}>
 
-      <div className="mb-16 mt-20 text-center w-full md:max-w-4xl mx-auto h-20">
+      <div className="mb-20 text-center w-full md:max-w-4xl mx-auto h-20">
 
         <h1 className="text-5xl font-medium text-neutral-900 dark:text-neutral-100 ">{title}</h1>
         <p className="text-lg font-medium text-neutral-600 dark:text-neutral-400 mt-4">{subtitle}</p>
--- src/components/sections/HomeSectionContainer.tsx
+++ src/components/sections/HomeSectionContainer.tsx
@@ -10,7 +10,7 @@ export default function Projects({title, children}: props) {
     return (
         <SectionContainer className="divide-neutral-700 my-5 md:my-20">
             <div className="flex flex-row items-center justify-evenly mb-14">
-                <hr className="hidden md:block w-1/2 my-4 border-neutral-700" />
+                <hr className="block w-1/2 my-4 border-neutral-700" />
                 <h1 className="text-3xl md:text-5xl font-medium text-gray-900 dark:text-gray-100">
                     {title}
                 </h1>
