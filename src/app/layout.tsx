9dfc9701ea467fbe5f843084c464bce8ee1f8ba0
Salvador <100873582+salvaKaraka@users.noreply.github.com>
2024-03-28 22:27:37 -0300

    Update layout.tsx to support google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import './globals.css'
 import Footer from '@/components/Footer'
 import Header from '@/components/header/Header'
+import Head from 'next/head'
 
 export const metadata: Metadata = {
   title: 'Salvador Karakachoff - Computer Engineering and Data Science',
@@ -16,8 +17,20 @@ export default function RootLayout({
 
   return (
     <html lang="en" className="dark">
-
       <body  className="bg-slate-100/90 dark:bg-neutral-900 overflow-x-hidden">
+
+        <Head>
+          <!-- Google tag (gtag.js) -->
+            <script async src="https://www.googletagmanager.com/gtag/js?id=G-7TFF89Y8XW"></script>
+            <script>
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+            
+              gtag('config', 'G-7TFF89Y8XW');
+            </script>
+        </Head>
+        
         <Header />
 
         {children}
