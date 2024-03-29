281b766cfb362b63834658abadeea6a9dcbc638f
Salvador <100873582+salvaKaraka@users.noreply.github.com>
2024-03-28 22:30:59 -0300

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from 'next'
 import './globals.css'
 import Footer from '@/components/Footer'
 import Header from '@/components/header/Header'
-import Head from 'next/head'
 
 export const metadata: Metadata = {
   title: 'Salvador Karakachoff - Computer Engineering and Data Science',
@@ -17,20 +16,6 @@ export default function RootLayout({
 
   return (
     <html lang="en" className="dark">
-
-              <Head>
-          <!-- Google tag (gtag.js) -->
-            <script async src="https://www.googletagmanager.com/gtag/js?id=G-7TFF89Y8XW"></script>
-            <script>
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-            
-              gtag('config', 'G-7TFF89Y8XW');
-            </script>
-        </Head>
-
-      
       <body  className="bg-slate-100/90 dark:bg-neutral-900 overflow-x-hidden"> 
         <Header />
 
